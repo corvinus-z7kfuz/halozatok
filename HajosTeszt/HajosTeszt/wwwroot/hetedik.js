f02dd761c9b61255c18f74b26a0dba9eee926db3
kecsooo <szermek.a@gmail.com>
2021-03-27 14:55:54 +0100

    félkész

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,20 @@
+﻿var kérdések;
+
+window.onload = function letöltés() {
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data));
+
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        console.log(d)
+        kérdések = d;
+    }
+}
+
+function kérdésMegjelenítés(kérdés) {
+    for (var i = 0; i < kérdés.length; i++) {
+        document.getElementById("kérdés_szöveg").innerHTML = kérdés[i].questionText;
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/";
+    }
+}
