16719789b21a59c8897e8aba2cd7bdde14ae334f
kecsooo <szermek.a@gmail.com>
2021-04-15 18:54:40 +0200

    controllerek és kérdések

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -12,13 +12,11 @@ namespace HajosTeszt
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,8 +25,9 @@ namespace HajosTeszt
             }
 
             app.UseHttpsRedirection();
+
             app.UseDefaultFiles();
-            app.UseStaticFiles();
+            app.UseStaticFiles(); //A sorrend fontos!
 
             app.UseRouting();
 
@@ -39,6 +38,12 @@ namespace HajosTeszt
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
